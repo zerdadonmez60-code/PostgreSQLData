dbad37c27432af5faa569115ee637d6f76909afc
Zerda Dönmez <zerdadonmez60@gmail.com>
2026-02-18 23:07:44 +0300

    query 4

---
+++ project/4_top_paying_skills.sql
@@ -0,0 +1,22 @@
+/*
+Answer: What are the most optimal skills to learn (aka it’s in high demand and a high-paying skill) for a data analyst? 
+Identify skills in high demand and associated with high average salaries for Data Analyst roles
+Concentrates on remote positions with specified salaries
+Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), offering strategic insights for career development in data analysis
+*/
+
+SELECT
+    skills,
+  ROUND(AVG(salary_year_avg), 0) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short = 'Data Analyst' 
+    AND salary_year_avg IS NOT NULL 
+    AND job_work_from_home = True
+GROUP BY
+    skills
+ORDER BY
+    avg_salary DESC
+LIMIT 25
